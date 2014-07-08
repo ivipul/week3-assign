f3386addc03a0071f215d129a040b5b5bcd543ea
ivipul <ivipul@ivipul-macbookpro.roam.corp.google.com>
2014-07-08 04:13:12 -0700

    Initial Commit

---
+++ week3-assign/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  week3-assign
+//
+//  Created by Vipul Thakur on 7/8/14.
+//  Copyright (c) 2014 ivipul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ week3-assignTests/week3_assignTests.m
@@ -0,0 +1,34 @@
+//
+//  week3_assignTests.m
+//  week3-assignTests
+//
+//  Created by Vipul Thakur on 7/8/14.
+//  Copyright (c) 2014 ivipul. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface week3_assignTests : XCTestCase
+
+@end
+
+@implementation week3_assignTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
